adbf8f0dae18d42e9eb503daa906179f0dcf966a
m.jibal82 <m.jibal82@gmail.com>
2026-07-30 09:51:21 +0700

    Update database: tambah kolom phone di tabel users

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -14,13 +14,15 @@ class AuthController extends Controller
         $request->validate([
             'name' => 'required|string',
             'email' => 'required|email|unique:users',
-            'password' => 'required|min:6'
+            'password' => 'required|min:6',
+            'phone' => 'required|string|max:20'
         ]);
 
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
-            'password' => Hash::make($request->password)
+            'password' => Hash::make($request->password),
+            'phone' => $request->phone
         ]);
 
         return response()->json([
